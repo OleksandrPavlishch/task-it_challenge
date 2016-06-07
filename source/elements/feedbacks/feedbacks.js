0c823b8d03351ca9e39a6f3b252bfbc0fed8d3ce
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2016-06-07 11:01:55 +0300

    feedback slider

--- source/elements/feedbacks/feedbacks.js
+++ source/elements/feedbacks/feedbacks.js
@@ -1,9 +1,18 @@
 'use strict';
 (function () {
 	$(".feedbacks_slider").owlCarousel({
-		items: 1
+		items: 1.5
 		, mouseDrag: false
-		, nav: true
 		, dots: false
+		, margin: 10
+		, center: true
+		, loop: true
+		, responsive: {
+			1026: {
+				items: 1
+				, margin: 0
+				, nav: true
+			}
+		}
 	});
 })();
