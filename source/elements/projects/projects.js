c7c53a986a88c64d7dffb18de0d54b71b49b6ce8
OleksandrPavlyshch <lokdawn80@gmail.com>
2016-06-09 21:46:33 +0300

    arrow for projects_slider-item-images

--- source/elements/projects/projects.js
+++ source/elements/projects/projects.js
@@ -2,28 +2,31 @@
 (function () {
 	$(".projects_slider").owlCarousel({
 		dots: false
-		, loop: true
 		, navContainerClass: "projects_slider-nav"
 		, navText: ''
 		, nav: false
-		, center: true
 		, responsive: {
 			1440: {
 				items: 5
 			}
 			, 1026: {
 				items: 4
+				, loop: false
 				, nav: true
 				, margin: 0
 				, center: false
 			}
 			, 760: {
-				items: 2
+				items: 2.5
 				, margin: 20
+				, center: true
+				, loop: true
 			}
 			, 320: {
 				items: 1.4
 				, margin: 10
+				, center: true
+				, loop: true
 			}
 		}
 	});
@@ -31,9 +34,12 @@
 	$(".projects_slider-item-images").owlCarousel({
 		items: 1
 		, mouseDrag: false
+		, touchDrag: false
 		, dots: false
-		, loop: false
+		, loop: true
 		, navText: ''
+		, nav: true
+		, navContainerClass: "projects_slider-item-images-nav"
 	});
 
 
