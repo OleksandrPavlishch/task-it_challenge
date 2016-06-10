5de2e9fd95a37871372438d12318d4cdbcabac9b
OleksandrPavlyshch <lokdawn80@gmail.com>
2016-06-10 12:28:02 +0300

    styling banner

--- source/elements/banner/banner.js
+++ source/elements/banner/banner.js
@@ -1,4 +1,18 @@
 'use strict';
 (function () {
-	
+	$(".banner_slider").owlCarousel({
+		items: 1
+		, mouseDrag: false
+		, dots: false
+		, center: true
+		, loop: true
+		, navText: ''
+		, responsive: {
+			1026: {
+				items: 1
+				, mouseDrag: false
+				, nav: true
+			}
+		}
+	});
 })();
