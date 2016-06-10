aa39577e385e6416c440c402fb15a43540a7d9e0
OleksandrPavlyshch <lokdawn80@gmail.com>
2016-06-10 12:27:12 +0300

    add scroll-to

---
+++ source/elements/scroll-to/scroll-to.js
@@ -0,0 +1,23 @@
+'use strict';
+(function () {
+
+	var $scrollLinks = $('.button_start')
+		, $htmlBody = $('html, body')
+		, $body = $('body')
+		, topBlockId = '#header'
+		, header = $('#header');
+
+	$scrollLinks.on('click', function(event) {
+		event.preventDefault();
+
+		var $this = $(this)
+			, link = $this.attr("href")
+			, section = $body.find(link)
+			, headerHeight = (link !== topBlockId) ? header.height() : 0;
+
+		if(section.length){
+			$htmlBody.animate({scrollTop: section.offset().top - headerHeight}, 1000);
+			return;
+		}
+	});
+})();
